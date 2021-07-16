3580e12e13634297ec43f71f590129734133fe3c
yfchua <35942337+yfchua@users.noreply.github.com>
2021-07-16 20:30:54 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,19 @@ radio.setGroup(1)
 radio.sendNumber(7)
 basic.forever(function () {
     if (GHBit.Rocker(GHBit.enRocker.Up)) {
-        radio.sendString("A")
+        radio.sendString("U")
         basic.showArrow(ArrowNames.North)
     } else if (GHBit.Rocker(GHBit.enRocker.Down)) {
-        radio.sendString("B")
+        radio.sendString("D")
         basic.showArrow(ArrowNames.South)
     } else if (GHBit.Rocker(GHBit.enRocker.Left)) {
-        radio.sendString("C")
+        radio.sendString("L")
         basic.showArrow(ArrowNames.West)
     } else if (GHBit.Rocker(GHBit.enRocker.Right)) {
-        radio.sendString("D")
+        radio.sendString("R")
         basic.showArrow(ArrowNames.East)
     } else if (GHBit.Rocker(GHBit.enRocker.Press)) {
-        radio.sendString("I")
+        radio.sendString("P")
         basic.showIcon(IconNames.Target)
     } else if (GHBit.Rocker(GHBit.enRocker.Nostate)) {
         radio.sendString("0")
@@ -24,21 +24,49 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (GHBit.Button(GHBit.enButton.B2, GHBit.enButtonState.Press)) {
-        radio.sendString("G")
+        radio.sendString("BU")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
 })
 basic.forever(function () {
     if (GHBit.Button(GHBit.enButton.B1, GHBit.enButtonState.Press)) {
-        radio.sendString("F")
+        radio.sendString("BF")
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # . . .
+            . # . . .
+            . . . . .
+            `)
     }
 })
 basic.forever(function () {
     if (GHBit.Button(GHBit.enButton.B4, GHBit.enButtonState.Press)) {
-        radio.sendString("E")
+        radio.sendString("BR")
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . # #
+            . . . # .
+            . . . . .
+            `)
     }
 })
 basic.forever(function () {
     if (GHBit.Button(GHBit.enButton.B3, GHBit.enButtonState.Press)) {
-        radio.sendString("H")
+        radio.sendString("BD")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # # # .
+            . . # . .
+            `)
     }
 })
